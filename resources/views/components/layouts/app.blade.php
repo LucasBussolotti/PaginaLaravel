22fb5a4a736491693f3f6c19d09900688f924fac
Lucas Bussolotti <lucasfbussolotti@gmail.com>
2023-05-16 01:34:26 -0300

    agregamos tailwind

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <title>{{ $title }}</title>
     </head>
